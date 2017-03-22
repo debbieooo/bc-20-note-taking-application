a2bd092e3a262442ad0fe151525d029f920c130d
Deborah Oni <deborahoooni@gmail.com>
2017-03-22 09:12:38 +0100

    app file created for functions

---
+++ app.js
@@ -0,0 +1,52 @@
+
+//var prompt = require('readline');
+var firebase= require('firebase');
+var readlineSync = require('readline-sync');
+
+
+/*const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});*/
+
+
+var config = {
+    apiKey: "AIzaSyCXblpDaPjl4y4GM-e4McTdgb_IX8oIokY",
+    authDomain: "projectx-8d97e.firebaseapp.com",
+    databaseURL: "https://projectx-8d97e.firebaseio.com",
+    storageBucket: "projectx-8d97e.appspot.com",
+    messagingSenderId: "1011421642320"
+};
+firebase.initializeApp(config);
+
+
+
+function createNote(arg,callback){
+
+	var content = arg.note_content;
+	var messageRef = firebase.database().ref().child('notes');
+
+
+	
+
+		messageRef.push().set({
+			content :content
+		});
+
+		console.log('Note created');
+		callback();
+	
+
+}
+
+
+module.exports = {
+
+    //searchNote: searchNote,
+    //listNotes: listNotes,
+    //deleteNote: deleteNote,
+    //viewNote: viewNote,
+    createNote: createNote
+
+}
+
