1b5a0818b0754daf531dd3b4e78ddfb70b2fa601
Deborah Oni <deborahoooni@gmail.com>
2017-03-24 03:22:36 +0100

    code clean up

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-//var prompt = require('readline');
 var firebase= require('firebase');
 var readlineSync = require('readline-sync');
 var jsonfile = require('jsonfile');
-var json2csv = require('json2csv');
 var fs = require('fs');
 var readlineSync = require('readline-sync');
-
-
-
 var config = {
     apiKey: "AIzaSyCXblpDaPjl4y4GM-e4McTdgb_IX8oIokY",
     authDomain: "projectx-8d97e.firebaseapp.com",
@@ -16,44 +11,59 @@ var config = {
     messagingSenderId: "1011421642320"
 };
 firebase.initializeApp(config);
-
 var messageRef = firebase.database().ref().child('notes');
 
-function createNote(arg,callback){
-
+function createNote(arg,callback) {
 	var content= readlineSync.question('Please enter your content:: ');
-	console.log(content);
-		messageRef.push().set({
-			content :content
-		});
-		console.log('Note created');
-		callback();
+	var reply;
+	while ((content.length<=0 ) || (content === 'exit') ) {
+		if (content === 'exit') {
+			reply= readlineSync.question('Do you want to exit?, Y for yes and N for no');
+			if (reply === 'Y') {
+				console.log("You will exit back to the menu")
+				callback();
+				break;
+			} else {
+				content = readlineSync.question('Please enter your content:: ');
+			}
+		} else	{
+			content= readlineSync.question('Please enter your content:: ');
+		}
+	}
+	if (reply !== 'Y') {
+			console.log(content);
+			messageRef.push().set({
+				content :content
+			});
+			console.log('Note created');
+			callback();
+	}
 }
-
-function viewNote(arg, callback){
+	
+function viewNote(arg, callback) {
   var id = arg.id;
-  messageRef.once("value", function(snapshot){
+  messageRef.once("value", function(snapshot) {
 	var i = 1;
     var arr = [""];
-    snapshot.forEach(function(childSnapshot){
+    snapshot.forEach(function(childSnapshot) {
       arr.push(childSnapshot.val().content);
     });
-    if(arr[id]){
+    if (arr[id]) {
       console.log(arr[id]);   
       callback();
-    }else {     
+    } else {     
       console.log(" "+"Not a valid key");
       callback();     
-    }
+     }
   });
 }
 
-function deleteNote(arg,callback){
+function deleteNote(arg,callback) {
 	var id = arg.id;	
-	messageRef.once("value", function(snapshot){
+	messageRef.once("value", function(snapshot) {
 		var i=1;		
-		snapshot.forEach(function(childSnapshot){			
-			if(i == id){
+		snapshot.forEach(function(childSnapshot) {			
+			if (i == id) {
 				console.log("Note: "+ childSnapshot.val().content);
 				childSnapshot.ref.remove();
 				console.log("Note deleted");
@@ -61,31 +71,22 @@ function deleteNote(arg,callback){
 			}
 			i++;
 		});
-			if(id>=i){
-
+			if (id>=i || id<1) {
 				console.log("Note index is not available. Viewing the note list first might help you confirm the right index");
-			}
-			else if(id<1){
-				console.log("Note index is not available. It should be greater than 0");
-			}		
+			} 		
 	});	
 	callback();
 }
 
-function listNotes(arg,callback){
+function listNotes(arg,callback) {
 	var id =1;
-
-	if(id>arg.options.limit){
-
+	if (id>arg.options.limit) {
 		console.log("Invalid limit, the note's full list will be displayed for you.");
-
 		callback();
 	}
-	if(!arg.options.limit){
-
-		messageRef.once('value',function(snapshot){
-
-			snapshot.forEach(function(childSnapshot){
+	if (!arg.options.limit) {
+		messageRef.once('value',function(snapshot) {
+			snapshot.forEach(function(childSnapshot) {
 				var child = childSnapshot.val();
 				console.log("");
 				console.log("   "+id+" "+child.content);
@@ -93,11 +94,9 @@ function listNotes(arg,callback){
 			});
 			callback();
 		});
-	}
-
-	else if(!isNaN(arg.options.limit)){
+	} else if (typeof arg.options.limit==='number') {
 		messageRef.limitToFirst(arg.options.limit).once('value', function(snapshot) {
-			snapshot.forEach(function(childSnapshot){
+			snapshot.forEach(function(childSnapshot) {
 				var child = childSnapshot.val();
 				console.log("");
 				console.log("   "+id+" "+child.content);
@@ -105,21 +104,19 @@ function listNotes(arg,callback){
 			});
 		callback();
 		});
-	}
-	else{
+	  } else {
 		console.log('Wrong query input, please try again');
 		callback();
-	}		
+	   }		
 }
 
-function searchNote(arg, callback){
+function searchNote(arg, callback) {
 	var i=1;
 	var search = arg.query_string;	
-	if(!isNaN(arg.options.limit)){
-		messageRef.limitToFirst(arg.options.limit).once('value',function(snapshot){
-			snapshot.forEach(function(childSnapshot){
+	if (typeof arg.options.limit === 'number') {
+		messageRef.limitToFirst(arg.options.limit).once('value',function(snapshot) {
+			snapshot.forEach(function(childSnapshot) {
       			if (childSnapshot.val().content.indexOf(arg.query_string) >-1) {
-      				//console.log('Note found');
 					console.log(i+" "+ childSnapshot.val().content );
 					callback();
 				}
@@ -127,13 +124,10 @@ function searchNote(arg, callback){
 			});
 		});
 		callback();
-	}
-
-	else if(!arg.options.limit){
+	} else if (!arg.options.limit) {
 		messageRef.once("value", function(snapshot){			
 			snapshot.forEach(function(childSnapshot){				
       			if (childSnapshot.val().content.indexOf(arg.query_string) > -1) {
-      				//console.log('Note found');
 					console.log(i+" "+ childSnapshot.val().content );
 					callback();
 				}
@@ -141,51 +135,26 @@ function searchNote(arg, callback){
 			});
 		});
 		callback();
-	}
+	  } else {
+		console.log("Wrong input");
+		callback();
+	   }
 }
 
-function jsonExport(callback){
- 		messageRef.once("value", function(snapshot){
+function jsonExport(callback) {
+ 		messageRef.once("value", function(snapshot) {
 		var file = snapshot.val();
 		var filename = "/Users/deborahoni/Desktop/Notes.json";
 		jsonfile.writeFile(filename,file,(err) => {		
-			if(!err){
+			if (!err) {
 				console.log("Export complete");
-			}
-			else{
+			} else {
 				console.log("An error occurred");
-			}
+			 }
 		})
 	});
-
 	callback();	
 }
-
-/*function csvExport(callback){	
-	messageRef.once("value", function(snapshot){
-		var file = snapshot.val();
-		var filename = "/Users/deborahoni/Desktop/Notes.csv";
-		console.log(file);
-		var fields=['snapshot.key()','content'];
-		try{
-			var result = json2csv({data: file ,fields : fields});
-			console.log(result);
-			fs.writeFile(filename,csv,(err) => {
-				if(!err){
-					console.log("Export complete");
-				}
-				else{
-					console.log("An error occurred");
-				}
-			});
-		}
-		catch(err){
-			console.log("Could not convert to csv");
-		}
-	});
-	callback();
-}*/
-
 
 module.exports = {
     searchNote: searchNote,
@@ -193,6 +162,6 @@ module.exports = {
     deleteNote: deleteNote,
     viewNote: viewNote,
     createNote: createNote,
-    jsonExport: jsonExport,
-    csvExport: csvExport
+    jsonExport: jsonExport
+    
 }
