7060a2d1511157b3be8c991cdb4d1875c26b1f13
Deborah Oni <deborahoooni@gmail.com>
2017-03-24 05:45:49 +0100

    final edge case change

--- main.js
+++ main.js
@@ -3,48 +3,43 @@ const vorpal= require('vorpal')();
 var app = require('./app.js');
 var figlet = require('figlet');
 var readlineSync = require('readline-sync');
+var content= readlineSync.question("Hey there! What's your name?   ");
+while(!isValidText(content)){
+	var content= readlineSync.question("Please try again. This is not a valid name. What's your name?  ");
+}
 
-
-
-var content= readlineSync.question("Hey there! What's your name?");
-
-figlet('Hi, '+ content, function(err,data){
+	figlet('Hi, '+ content, function(err,data){
 	if(err){
 		console.log('Something went wrong..');
 		return;
 	}
-
 	console.log(data)
+	});
 
-});
+function isValidText(content){
+	var regex = /^[a-zA-Z][a-zA-Z\s]*$/;
+	return regex.test(content);
+}
 
-console.log("Please enter help to view the options available");
-
-
-
+console.log("     Please enter help to view the options available  ");
 vorpal
 	.command('createnote')
 	.description('Saves the notes to the database')
 	.action(function(arguments,callback){
 		app.createNote(arguments,callback);
 });
-
-
 vorpal
 	.command('viewnote <id>')
 	.description('To view already stored notes')
 	.action(function(arguments,callback){
 		app.viewNote(arguments,callback);
 });
-
-
 vorpal
 	.command('deletenote <id>')
 	.description('Deletes already stored notes')
 	.action(function(arguments,callback){
 		app.deleteNote(arguments,callback);
 });
-
 vorpal
   .command('searchnotes <query_string>')
   .description('It searches a note for a query string')
@@ -52,8 +47,6 @@ vorpal
   .action(function(arguments, callback) {
     app.searchNote(arguments, callback);
 });
-
-
 vorpal
   .command('listnotes')
   .description('use this with the --limit to return a number of note entries.')
@@ -61,15 +54,6 @@ vorpal
   .action(function(arguments, callback) {
     app.listNotes(arguments, callback);
 });
-
-/*vorpal
-
-	.command('next')
-	.description('')
-	.action(function(arguments,callback){
-		app.next(arguments,callback);
-});*/
-
 vorpal
   .command('Export JSON')
   .description('Use this to export files in JSON format ')
@@ -78,19 +62,9 @@ vorpal
 });
 
 vorpal
-  .command('Export CSV')
-  .description('Use this to export files in CSV format ')
-  .action(function(arguments, callback) {
-    app.csvExport(callback);
-});
-
-
-vorpal
 
 	.delimiter('Notes==>')
 	.show();
-
-
 
 
 
